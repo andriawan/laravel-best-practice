347c518f8f0b3adab50d2c212c63a939a659bf44
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-07-10 15:47:27 +0000

    feat(swagger): add swagger with base config

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Services\AuthService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use OpenApi\Attributes as OA;
 
+#[OA\Tag(
+    name: "auth",
+    description: "Operations about user",
+)]
 class AuthController extends Controller
 {
     private AuthService $authService;
@@ -18,6 +22,20 @@ class AuthController extends Controller
     /**
      * Auth based on pass and email
      */
+    #[OA\Post(
+        path: "/api/auth/login",
+        summary: "Logs user into the system",
+        tags: ["auth"],
+        security: [
+            [
+                "basicAuth" => [],
+            ],
+        ],
+        responses: [
+            new OA\Response(response: 200, description: "successful operation"),
+            new OA\Response(response: 400, description: "Invalid username/password supplied"),
+        ]
+    )]
     public function authenticate(Request $request)
     {
         return $this->authService->authenticate($request);
@@ -26,6 +44,28 @@ class AuthController extends Controller
     /**
      * logout
      */
+    #[OA\Post(
+        path: "/api/auth/logout",
+        summary: "Logs out current logged in user session",
+        tags: ["auth"],
+        security: [
+            [
+                "bearerAuth" => [],
+            ],
+        ],
+        requestBody: new OA\RequestBody(
+            content: new OA\JsonContent(
+                properties: [
+                    new OA\Property(property: "token", type: "string"),
+                    new OA\Property(property: "refresh_token", type: "string"),
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(response: 200, description: "successful operation"),
+            new OA\Response(response: 401, description: "unauthorized"),
+        ]
+    )]
     public function logout(Request $request)
     {
         return $this->authService->logout($request);
@@ -34,6 +74,22 @@ class AuthController extends Controller
     /**
      * refresh
      */
+    #[OA\Post(
+        path: "/api/auth/token/refresh",
+        summary: "refresh token",
+        tags: ["auth"],
+        requestBody: new OA\RequestBody(
+            content: new OA\JsonContent(
+                properties: [
+                    new OA\Property(property: "refresh_token", type: "string"),
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(response: 200, description: "successful operation"),
+            new OA\Response(response: 401, description: "unauthorized"),
+        ]
+    )]
     public function refreshToken(Request $request)
     {
         return $this->authService->refreshToken($request);
@@ -42,6 +98,20 @@ class AuthController extends Controller
     /**
      * get user profile
      */
+    #[OA\Get(
+        path: "/api/auth/me",
+        summary: "get user profile",
+        tags: ["auth"],
+        security: [
+            [
+                "bearerAuth" => [],
+            ],
+        ],
+        responses: [
+            new OA\Response(response: 200, description: "successful operation"),
+            new OA\Response(response: 401, description: "unauthorized"),
+        ]
+    )]
     public function getUserProfile(Request $request)
     {
         return $this->authService->getUserProfile($request);
@@ -50,6 +120,27 @@ class AuthController extends Controller
     /**
      * Note to do: for admin only or high privilege
      */
+    #[OA\Post(
+        path: "/api/auth/blacklist",
+        summary: "blacklist token for user",
+        tags: ["auth"],
+        security: [
+            [
+                "bearerAuth" => [],
+            ],
+        ],
+        requestBody: new OA\RequestBody(
+            content: new OA\JsonContent(
+                properties: [
+                    new OA\Property(property: "sub", type: "string"),
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(response: 200, description: "successful operation"),
+            new OA\Response(response: 401, description: "unauthorized"),
+        ]
+    )]
     public function blacklistTokenForUser(Request $request)
     {
         return $this->authService->blacklistTokenByUserId($request);
